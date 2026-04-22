e737eca75e6685a970a3108411c97e33c783dad7
bossadizenith <org.codeen@gmail.com>
2026-04-22 05:49:50 +0100

    refactor: reorder import statements in app.ts for consistency

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,11 +1,11 @@
-import "./utils/load-env.js";
 import { db } from "@lite/db";
+import { env } from "@lite/env/server.js";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { handle } from "hono/vercel";
 import { routes } from "./routes/index.js";
 import type { ReqVariables } from "./utils/hono.js";
-import { cors } from "hono/cors";
-import { env } from "@lite/env/server.js";
+import "./utils/load-env.js";
 
 const app = new Hono<{ Variables: ReqVariables }>();
 
