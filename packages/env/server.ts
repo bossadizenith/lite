162f9fb19684095641c6e3d73d11b3f7276631c8
bossadizenith <org.codeen@gmail.com>
2026-04-23 03:37:13 +0100

    feat: add AWS and ECS environment configuration variables to server schema and turbo pipeline

--- packages/env/server.ts
+++ packages/env/server.ts
@@ -5,11 +5,20 @@ import BASE_ENV from "./base.js";
 export const env = createEnvCore({
   ...BASE_ENV,
   runtimeEnv: process.env,
-
   server: {
     DATABASE_URL: z.url(),
     BACKEND_URL: z.string(),
     FRONTEND_URL: z.string(),
+    AWS_ACCESS_KEY_ID: z.string(),
+    AWS_SECRET_ACCESS_KEY: z.string(),
+    AWS_REGION: z.string(),
+    // REDIS_URL: z.string(),
+    ECS_CLUSTER_ARN: z.string(),
+    ECS_TASK_DEFINITION_ARN: z.string(),
+    SUBNET_1: z.string(),
+    SUBNET_2: z.string(),
+    SUBNET_3: z.string(),
+    SECURITY_GROUP: z.string(),
     TRUSTED_ORIGINS: z
       .string()
       .transform((val) => val.trim())
